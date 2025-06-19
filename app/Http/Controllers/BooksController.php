786641e13083c50b449dd1d6bb89231a9402de11
sokchea-boy <sokchea.boy@student.passerellesnumeriques.org>
2025-06-19 09:42:34 +0700

    added book conece with data base mysql to book-v1

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -3,29 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Book;
 
 class BooksController extends Controller
 {
-    public $BookObject = [
-        [
-            "id" => "01",
-            "title" => "Sample Book 1",
-            "author" => "Chhea", 
-            "isbn" => "1234567890123",
-            "publicationYear" => 2020, 
-            "genre" => "Fiction",
-            "availableCopies" => 5
-        ],
-        [
-            "id" => "02", 
-            "title" => "Sample Book 2",
-            "author" => "Sang  Meng",
-            "isbn" => "9876543210987",
-            "publicationYear" => 2021,
-            "genre" => "Non-Fiction",
-            "availableCopies" => 3
-        ]
-    ];
+   
     /**
      * Display a listing of the resource.
      */
@@ -33,22 +15,21 @@ class BooksController extends Controller
     {
         return response()->json([
             'message' => 'Request successful',
-            'data' => $this->BookObject
+            'data' => Book::all(),
         ], 200);
     }
       /**
      * Store a newly created resource in storage (alias for store).
      */
     public function create( Request $request){
-          $newBook = [
-            'id' => $request->id,
+          $newBook = Book::create([
             'title' => $request->title,
             'author' => $request->author,
             'isbn' => $request->isbn,
             'publicationYear' => $request->publicationYear,
             'genre' => $request->genre,
             'availableCopies' => $request->availableCopies
-        ];
+        ]);
 
         return response()->json([
             'message' => 'successfully created',
@@ -56,50 +37,21 @@ class BooksController extends Controller
         ]);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-   public function store(Request $request)
-    {
-
-        // Create new book
-        $newBook = [
-            'id' => $request->id,
-            'title' => $request->title,
-            'author' => $request->author,
-            'isbn' => $request->isbn,
-            'publicationYear' => $request->publicationYear,
-            'genre' => $request->genre,
-            'availableCopies' => $request->availableCopies
-        ];
-
-        $this->BookObject[] = $newBook;
-
-        return response()->json([
-            'message' => 'Successfully created',
-            'data' => $newBook
-        ], 201);
-    }
+    
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show( $id)
     {
-        $book = null;
-        foreach ($this->BookObject as $item) {
-            if ($item['id'] === $id) { 
-                $book = $item;
-                break;
-            }
-        }
 
-        if (!$book) {
+        $book = Book::find($id);
+
+        if (!$book){
             return response()->json([
                 'message' => 'Book not found'
-            ], 404);
+            ],404);
         }
-
         return response()->json([
             'message' => 'Book found',
             'data' => $book
@@ -110,53 +62,34 @@ class BooksController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function edit( string $id, Request $request)
+    public function edit( Request $request, $id)
     {
-        $index = null;
-        foreach($this->BookObject as $key => $item){
-            if ($item['id'] === $id){
-                $index = $key;
-            }
-        }
-         // Check if book exists
-        if ($index === null) {
-            return response()->json([
-                'message' => 'Book not found'
-            ], 404);
-        }
-        $updatedBook = array_merge($this->BookObject[$index], $request->only([
-            'title',
-            'author',
-            'isbn',
-            'publicationYear',
-            'genre',
-            'availableCopies'
-        ]));    
-        $this->BookObject[$index] = $updatedBook;
-        return response()->json([
-            'message' => 'successfully update',
-            'data' => $updatedBook
+       $newBook = Book::find($id);
+       
+       $newBook->update([
+             'id' => $request->id,
+            'title' => $request->title,
+            'author' => $request->author,
+            'isbn' => $request->isbn,
+            'publicationYear' => $request->publicationYear,
+            'genre' => $request->genre,
+            'availableCopies' => $request->availableCopies
+       ]);
 
-        ], 200);
+       return response()->json([
+        'message' => 'Book update successfully',
+        'data' => $newBook,
+       ],200);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function delete(string $id)
+    public function delete($id)
     {
-        $index = null;
-        foreach ($this-> BookObject as $key => $item){
-            if ($item['id'] === $id){
-                $index = $key;
-                break;
-            }
-        }
-        if ($index === null){
-            return response()->json([
-                 'message'=> 'Book not found'
-            ], 404);
-        }
+       $Book = Book::where($id);
+       $Book->delete();
+       
 
         return response()->json([
             'message' => 'Delete successfully'
---
+++ database/migrations/2025_06_19_011002_create_books_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('title',150);
+            $table->string('author',150);
+            $table->string('isbn',250);
+            $table->integer('publicationYear');
+            $table->string('genre',250);
+            $table->integer('availableCopies',);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
